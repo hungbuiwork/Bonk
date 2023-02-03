a7c69596ab071ade8bea588563ea69322da84671
267thao <267thao@gmail.com>
2023-02-03 13:09:17 -0800

    added basic script for controlling round

---
+++ Assets/Scripts/RoundController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundController : MonoBehaviour
+{
+
+    public float roundTime;
+    public float timeVariable;
+
+    
+    public bool isBattlePhase;
+    public bool isPrepPhase; // prep phase
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isBattlePhase = false;
+        isPrepPhase = true;
+
+        timeVariable = Time.time + roundTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // prep phase starts
+        if(isPrepPhase)
+        {
+            if(Time.time >= timeVariable)
+            {
+                isPrepPhase = false;
+                isBattlePhase = true;
+                return;
+            }
+
+            // organize troops...
+
+        // battle phase    
+        }
+        else if(isBattlePhase)
+        {
+            // battling...
+        }
+
+    }
+}
