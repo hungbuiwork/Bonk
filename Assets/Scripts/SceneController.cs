4d537d96ca9cd93937102b5b4bd98caa55778f95
267thao <267thao@gmail.com>
2023-02-06 11:12:51 -0800

    added quit button

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitcher : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
-    public void PlayGame()
+    public void QuitGame()
+    {
+        Debug.Log("QUIT_GAME");
+        Application.Quit();
+    }
+
+    public void NextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void Back()
+    public void PreviousScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+
 }
