14fb9f6859f4aaa2bdcccec727a438b2a48c5fb7
hungbuiwork <hungbuiwork@gmail.com>
2023-02-06 18:12:28 -0800

    UnitManager now passes enemy troops list in UpdateTroops and UpdateBuildings

--- Assets/Scripts/MonoScripts/ShopTroops.cs
+++ Assets/Scripts/MonoScripts/ShopTroops.cs
@@ -6,12 +6,14 @@ public class ShopTroops : Shop
 {
     [SerializeField]
     private Transform cursor;
+    private int team; //0 or 1, depending on which team
     public override bool PurchaseCurrent(Inventory inventory)
     {
         bool result = base.PurchaseCurrent(inventory);
         if (result)
         {
             Instantiate(GetCurrent().Content, cursor.position, Quaternion.identity);
+            //TODO: Initialize the team of the spawned game object. 
         }
         return result;
     }
