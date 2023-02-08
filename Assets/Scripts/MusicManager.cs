75da4d8c0618f1fd6b8add08521a6efb50746f17
hungbuiwork <hungbuiwork@gmail.com>
2023-02-08 14:48:14 -0800

    small reorganization of scripts

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Manages different audiosources, and plays/pauses them accordingly. 
+    /// Uses the Observer pattern to listen to when the battle begins /ends
+    /// </summary>
     [SerializeField]
     private AudioSource standbyMusic;
     [SerializeField]
@@ -18,8 +21,6 @@ public class MusicManager : MonoBehaviour
         battleMusic.Pause();
     }
 
-
-    // Update is called once per frame
     private void SwitchBattleMusic() 
     {
         standbyMusic.Pause();
